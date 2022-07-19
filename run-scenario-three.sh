55b36e48407f7a3f8d101613976dd2974adebed3
Matteo Bordin <45484331+matbord@users.noreply.github.com>
2022-07-19 18:53:04 -0400

    added scenario three (#83)
    * added scenario three
    
    * delete dead code
    
    add copyright 
    run = true
    create print scenarios 1 + 3
    
    * increase font size
    
    * increase size
    
    * increase font ue
    
    * changed map size
    
    * delete traffic model from scenario three
    
    delete useless fiels
    add run script for scenario three
    
    * format traffic model correctly
    
    * modified:   .gitignore
    	modified:   run-scenario-three.sh
    	modified:   run-simulations.sh
    
    * Fix handover failure for energy saving use case (#81)
    
    * decrease ra preambles
    
    * cleared inconsistencies with numberOfRaPreambles
    
    * fix script
    
    * ra preamble just on mmwave
    
    * fix bad configure command
    
    * changes to run-simulations
    
    * add back lte helper and remove verbose logs
    
    * delete useless spaces
    
    * fix zero values
    
    * rollback of run-simulations
    
    Co-authored-by: matbord <matbord97@gmail.com>
    Co-authored-by: Andrea Lacava <lacava.a@northeastern.edu>
    Co-authored-by: mychele <michele.polese@gmail.com>

--- run-scenario-three.sh
+++ run-scenario-three.sh
@@ -7,11 +7,10 @@ e2nrEnabled=1 # enable e2 reports from nr secondary cells
 e2du=1 # enable reporting of DU PM containers
 e2cuUp=1 # enable reporting of CU UP PM containers
 e2cuCp=1 # enable reporting of CU CP PM containers
-trafficModel=3 # Type of the traffic model at the transport layer [0,3], can generate full buffer traffic (0), half nodes in full buffer and half nodes in bursty (1), bursty traffic (2), mixed setup (3)
 configuration=0 # 0: NR carrier at 850 MHz, low traffic | 1: NR carrier at 3.5 GHz, low traffic | 2: NR carrier at 28 GHz, high traffic
 minSpeed=2.0 # minimum UE speed in m/s
 maxSpeed=4.0 # maximum UE speed in m/s
-simTime=1.5 # simulation time
+simTime=1 # simulation time
 e2TermIp="10.102.157.65" # actual E2term IP interface
 
 # Useful parameters to be configured
@@ -22,49 +21,33 @@ EnableE2FileLogging=1 # enable offline generation of data
 ues=3 # Number of UEs for each mmWave ENB
 
 # Select 0 or 1 to switch between the optimized or debug build
-build=0
-builf_conf=0
-
-if [[ build -eq 1 ]];then
-  if [[ build_conf -eq 0 ]];then
-    # Debug build
-    echo "Build ns-3 in debug mode"
-    ./waf configure --build-profile=debug --out=build/debug
-  else
-      # Optimized build
-    echo "Build ns-3 in optimized mode"
-      ./waf configure --build-profile=optimized --out=build/optimized
-  fi
-fi
-
-# Select 0 or 1 to switch between the use cases
-# Remember to create an empty version of the control file before the start of this script, otherwise it would lead to premature crashes.
-use_case=1
-if [[ use_case -eq 0 ]];then
-  ## Energy Efficiency use case
-  echo "Energy Efficiency use case"
-  outageThreshold=-5.0 # use -5.0 when handover is not in NoAuto 
-  handoverMode="DynamicTtt"
-  indicationPeriodicity=0.02 # value in seconds (20 ms)
-  controlPath="es_actions_for_ns3.csv" # TS control file path
-  numberOfRaPreambles=20
+build=1
+if [[ build -eq 0 ]];then
+  # Debug build
+   echo "Build ns-3 in debug mode"
+   ./waf -debug
 else
-  ## Traffic Steering use case
-  echo "Traffic Steering use case"
-  outageThreshold=-1000
-  handoverMode="NoAuto"
-  indicationPeriodicity=0.1 # value in seconds (100 ms)
-  controlPath="ts_actions_for_ns3.csv" # EE control file path
-  numberOfRaPreambles=40
+    # Optimized build
+   echo "Build ns-3 in optimized mode"
+    ./waf -optimized
 fi
 
-#  NS_LOG="LteEnbNetDevice:LteEnbRrc:LteUeRrc:McEnbPdcp:McUePdcp" 
+# Remember to create an empty version of the control file before the start of this script, otherwise it would lead to premature crashes.
+
+## Energy Efficiency use case
+echo "Energy Efficiency use case"
+outageThreshold=-5.0 # use -5.0 when handover is not in NoAuto 
+handoverMode="DynamicTtt"
+indicationPeriodicity=0.02 # value in seconds (20 ms)
+controlPath="es_actions_for_ns3.csv" # ES control file path
+
+# NS_LOG="KpmIndication"
+# NS_LOG="RicControlMessage" 
 
 for i in $(seq 1 $N); do
   echo "Running simulation $i out of $N";
-  ./waf --run "scratch/scenario-one --RngRun=$i \
+  ./waf --run "scratch/scenario-three --RngRun=$i \
                                     --configuration=$configuration \
-                                    --trafficModel=$trafficModel \
                                     --enableTraces=$enableTraces \
                                     --e2lteEnabled=$e2lteEnabled \
                                     --e2nrEnabled=$e2nrEnabled \
@@ -81,7 +64,6 @@ for i in $(seq 1 $N); do
                                     --enableE2FileLogging=$EnableE2FileLogging \
                                     --minSpeed=$minSpeed\
                                     --maxSpeed=$maxSpeed\
-                                    --numberOfRaPreambles=$numberOfRaPreambles\
                                     --ns3::LteEnbNetDevice::E2Periodicity=$indicationPeriodicity\
                                     --ns3::MmWaveEnbNetDevice::E2Periodicity=$indicationPeriodicity\
                                     --ns3::LteEnbNetDevice::ControlFileName=$controlPath";
